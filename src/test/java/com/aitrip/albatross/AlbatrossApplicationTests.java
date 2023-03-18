ebccc70b55fc2089279879520b9930e290a98b41
madull1220@gmail.com <holdfishpalm==0>
2023-03-18 14:14:06 +0800

    Initial commit

---
+++ src/test/java/com/aitrip/albatross/AlbatrossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitrip.albatross;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbatrossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
